d50806845df1634db45ba9aab1fa1ae8104496cd
hf2020520 <1968292743@qq.com>
2017-11-08 11:19:41 +0800

    退出功能实现

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,7 @@
 (function () {
     window.CHAT = {
+        msgObj: document.getElementById("message"),
+        username : null,
         //实现登录功能
         usernameSubmit: function () {
             var username = document.getElementById("username").value;
@@ -7,8 +9,17 @@
                 document.getElementById("username").value = "";
                 document.getElementById("loginbox").style.display = "none";
                 document.getElementById("chatbox").style.display = "block";
+                this.init(username);
             }
             return false;
+        },
+        //退出功能实现
+        logout:function () {
+            location.reload();
+        },
+        init:function (username) {
+            this.username = username;
+            document.getElementById("showusername").innerHTML = this.username;
         }
     }
 })();
